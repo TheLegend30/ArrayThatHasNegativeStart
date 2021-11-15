1e352b0eb6be13f413ba83643f1ec58e1b5fa8cf
thelegend30 <luibichenko.oleg@gmail.com>
2021-11-16 01:38:47 +0200

    Preclass

---
+++ src/com/company/NegArray.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class NegArray <T>{
+    private int startIndex;
+    private int endIndex;
+    private T arr[];
+
+    public NegArray(int startIndex,int endIndex){
+
+    }
+}
